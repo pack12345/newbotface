35f6db041a480b0a5457ae59eb3e2d5599376bef
pack12345 <anurakintachua@gmail.com>
2017-05-03 11:00:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			// $url = 'https://api.line.me/v2/bot/message/reply';
-			$url = 'http://Administrator:8070/VoxeoCXP/DialogMapping?VSN';
+			$url = 'https://api.line.me/v2/bot/message/reply';
 			
 			$data = [
 				'replyToken' => $replyToken,
