782a989a4a10f4b3d961ad679f5c806c5db674e1
pack12345 <anurakintachua@gmail.com>
2017-05-05 11:29:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -25,11 +25,11 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			// $url = 'https://api.line.me/v2/bot/message/reply';
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			
+			$url = 'http://58.82.133.74:8099/VoxeoCXP/DialogMapping?VSN=MessageProxy&phone=0847685368&message=Test&User-Agent=MessageMedia';
+			$cxpMsg = http_get($url);
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$cxpMsg],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
