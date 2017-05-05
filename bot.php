629d8f6044adf9a987f4e365b4d9653d4436b254
pack12345 <anurakintachua@gmail.com>
2017-05-05 11:59:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,6 +31,7 @@ if (!is_null($events['events'])) {
 			
 			// Make a POST Request to Messaging API to reply to sender
 			 $url = 'https://api.line.me/v2/bot/message/reply';
+			error_log('cccccccccccccccccccccc');
 			//
 			//echo $url;
 			//$cxpMsg = $_GET($url);
