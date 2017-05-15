8a53feb1c16bfd4622a75dc8197e50cdde16530f
pack12345 <anurakintachua@gmail.com>
2017-05-16 01:23:34 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -52,19 +52,19 @@ if (!is_null($events['events'])) {
 			$messages=['type'=> 'template',
 					'altText' => 'this is a buttons template',
 					'template' => [
-							'type'=> 'buttons',
-							'thumbnailImageUrl'=> 'https://www.mx7.com/i/b7e/HdD0Yj.jpg',
-							'title' => 'Today - 30 Jun 17',
-							'text' => 'Everyday Special, Get Cash Back Up to 17 precentage',
-							'actions' => [
+						'type'=> 'buttons',
+						'thumbnailImageUrl'=> 'https://www.mx7.com/i/b7e/HdD0Yj.jpg',
+						'title' => 'Today - 30 Jun 17',
+						'text' => 'Everyday Special, Get Cash Back Up to 17 precentage',
+						'actions' => [
 
-									 ['type' => 'uri',
-										'label' => 'More information',
-										'uri' => 'https://www.kasikornbank.com/EN/promotion/Pages/Supermarket.aspx'
-									  ]
-								     ]
+								 ['type' => 'uri',
+									'label' => 'More information',
+									'uri' => 'https://www.kasikornbank.com/EN/promotion/Pages/Supermarket.aspx'
+								  ]
+							     ]
 
-								]
+							]
 				];
 			}	
 			 else if(substr($xcpResult,0,27)=="  https://www.mx7.com/i/1aa"){
