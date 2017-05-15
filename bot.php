ed63d3e31d79d029f09101f995f95f5b345e5bbc
pack12345 <anurakintachua@gmail.com>
2017-05-15 19:56:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -54,7 +54,10 @@ if (!is_null($events['events'])) {
 						'type'=> 'buttons',
 						'thumbnailImageUrl'=> 'https://www.mx7.com/i/b7e/HdD0Yj.jpg',
 						'title' => 'Today - 30 Jun 17',
-						'text' => 'Everyday Special',
+						'text' => 'Everyday Special, Get Cash Back Up to 17%
+							Get Cash Back up to 7%* when spending at 1,500 Baht or more / sales slip 
+							7% cash back for THE WISDOM card
+							4% cash back for other K-Credit Cards',
 						'actions' => [
 							[
 							    'type' => 'postback',
@@ -63,8 +66,8 @@ if (!is_null($events['events'])) {
 							  ],
 							  [
 							    'type' => 'uri',
-							    'label' => 'View detail',
-							    'uri' => 'http://example.com/page/123'
+							    'label' => 'More information',
+							    'uri' => 'https://www.kasikornbank.com/EN/promotion/Pages/Supermarket.aspx'
 							  ]
 						      ]
 
