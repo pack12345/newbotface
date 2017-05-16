c1be0e46f0f0a8611bb7510c152bff9ff7f996fa
pack12345 <anurakintachua@gmail.com>
2017-05-16 23:14:13 +0700

    Update bot_facebook.php

--- bot_facebook.php
+++ bot_facebook.php
@@ -66,63 +66,70 @@ $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@Syste
  			error_log('pic');
  			$messages=[
  				'attachment' =>['type' => 'template',
- 						      'elements' => [
- 								 [
- 									'title' => 'Shori Sushi House',
- 									'image_url'=> 'https://www.mx7.com/i/b7b/CQ6y5K.png',
- 									'subtitle' => 'Tel: 02-169-1532
-											Opening Hours: 11:00 – 23:00',
- 									'buttons' => [
- 										['type' => 'web_url',
- 										'title' => 'Location',
- 										'url' => 'https://goo.gl/maps/jxgfN1aXYzR2'
- 										]
+						'payload' => ['template_type' => 'generic',
+								'elements' => [
+										 [
+											'title' => 'Shori Sushi House',
+											'image_url'=> 'https://www.mx7.com/i/b7b/CQ6y5K.png',
+											'subtitle' => 'Tel: 02-169-1532
+													Opening Hours: 11:00 – 23:00',
+											'buttons' => [
+												['type' => 'web_url',
+												'title' => 'Location',
+												'url' => 'https://goo.gl/maps/jxgfN1aXYzR2'
+												]
+											]
+										]
  									]
- 								]
- 							]
-						    
+							      
+							      ]
+
  						]
  				];
 	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/bde'){
 		error_log('pic');
 		$messages=[
 			'attachment' =>['type' => 'template',
-					      'elements' => [
-							 [
-								'title' => 'Hakone Bangkok',
-								'image_url'=> 'https://www.mx7.com/i/bde/2oPh6u.png',
-								'subtitle' => 'Tel: 02-108-2790
-										Opening Hours: 10:00 – 22:00',
-								'buttons' => [
-									['type' => 'web_url',
-									'title' => 'Location',
-									'url' => 'https://goo.gl/maps/EJQUDLLtugE2'
+					'payload' => ['template_type' => 'generic',
+						      		'elements' => [
+										[
+											'title' => 'Hakone Bangkok',
+											'image_url'=> 'https://www.mx7.com/i/bde/2oPh6u.png',
+											'subtitle' => 'Tel: 02-108-2790
+													Opening Hours: 10:00 – 22:00',
+											'buttons' => [
+												['type' => 'web_url',
+												'title' => 'Location',
+												'url' => 'https://goo.gl/maps/EJQUDLLtugE2'
+												]
+											]
+										]
 									]
-								]
-							]
-						]
-
+						      
+						      ]
 					]
 			];
 	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/1f1'){
 	error_log('pic');
 	$messages=[
 		'attachment' =>['type' => 'template',
-				      'elements' => [
-						 [
-							'title' => 'Yoshino Yama',
-							'image_url'=> 'https://www.mx7.com/i/1f1/79drKy.png',
-							'subtitle' => 'Tel: 02-259-2582
-									Opening Hours: 17:00 – 01:00',
-							'buttons' => [
-								['type' => 'web_url',
-								'title' => 'Location',
-								'url' => 'https://goo.gl/maps/FgVPc8yddDo'
+				'payload' => ['template_type' => 'generic',
+					      	'elements' => [
+								 [
+									'title' => 'Yoshino Yama',
+									'image_url'=> 'https://www.mx7.com/i/1f1/79drKy.png',
+									'subtitle' => 'Tel: 02-259-2582
+											Opening Hours: 17:00 – 01:00',
+									'buttons' => [
+										['type' => 'web_url',
+										'title' => 'Location',
+										'url' => 'https://goo.gl/maps/FgVPc8yddDo'
+										]
+									]
 								]
 							]
-						]
-					]
-
+					      
+					      ]
 				]
 		];
 						
