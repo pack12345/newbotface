a7352435ca6784d820bf2c547bfcf353fa8fa87e
pack12345 <anurakintachua@gmail.com>
2017-05-16 16:46:52 +0700

    Update bot_facebook.php

--- bot_facebook.php
+++ bot_facebook.php
@@ -75,7 +75,7 @@ $jsonData = [
     ]
 ];
 //Encode the array into JSON.
-$jsonDataEncoded = $jsonData;
+$jsonDataEncoded = json_encode($jsonData);
 //Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
 //Attach our encoded JSON string to the POST fields.
