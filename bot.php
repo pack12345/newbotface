7456a3b7a8c11102ca93a5f82f17bf51611cc0c3
pack12345 <anurakintachua@gmail.com>
2017-05-17 10:07:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		error_log('userid  '.$event['source'].userId);
+		error_log('userid  '.$event['source']['userId']);
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent			
 			error_log('message '.$event['message']);
