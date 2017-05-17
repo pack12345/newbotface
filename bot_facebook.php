aa95e4d838115bf3bf78f95a2f56b9777d779e16
pack12345 <anurakintachua@gmail.com>
2017-05-17 22:49:52 +0700

    Update bot_facebook.php

--- bot_facebook.php
+++ bot_facebook.php
@@ -79,12 +79,17 @@ $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@Syste
 										 [
 											'title' => 'Shori Sushi House',
 											'image_url'=> 'https://www.mx7.com/i/b7b/CQ6y5K.png',
-											'subtitle' => 'Opening Hours: 11:00 – 23:00  Tel: 02-169-1532',
+											'subtitle' => 'Opening Hours: 11:00 – 23:00',
 											'buttons' => [
 												['type' => 'web_url',
 												'title' => 'Location',
 												'url' => 'https://goo.gl/maps/jxgfN1aXYzR2'
-												]
+												],
+												[
+												  'type' => 'phone_number',
+												  'title' => 'Call',
+												  'payload' => '+662-169-1532'
+											       ]
 											]
 										]
  									]
@@ -102,12 +107,17 @@ $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@Syste
 										[
 											'title' => 'Hakone Bangkok',
 											'image_url'=> 'https://www.mx7.com/i/bde/2oPh6u.png',
-											'subtitle' => 'Opening Hours: 10:00 – 22:00  Tel: 02-108-2790',
+											'subtitle' => 'Opening Hours: 10:00 – 22:00',
 											'buttons' => [
 												['type' => 'web_url',
 												'title' => 'Location',
 												'url' => 'https://goo.gl/maps/EJQUDLLtugE2'
-												]
+												],
+												[
+												  'type' => 'phone_number',
+												  'title' => 'Call',
+												  'payload' => '+662-108-2790'
+											       ]
 											]
 										]
 									]
@@ -124,12 +134,17 @@ $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@Syste
 								 [
 									'title' => 'Yoshino Yama',
 									'image_url'=> 'https://www.mx7.com/i/1f1/79drKy.png',
-									'subtitle' => 'Opening Hours: 17:00 – 01:00  Tel: 02-259-2582',
+									'subtitle' => 'Opening Hours: 17:00 – 01:00',
 									'buttons' => [
 										['type' => 'web_url',
 										'title' => 'Location',
 										'url' => 'https://goo.gl/maps/FgVPc8yddDo'
-										]
+										],
+										[
+										  'type' => 'phone_number',
+										  'title' => 'Call',
+										  'payload' => '+662-259-2582'
+									       ]
 									]
 								]
 							]
