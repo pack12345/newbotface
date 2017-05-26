a43497e9c0cf8dbdf4fe8c47995328fa7b5911d4
pack12345 <anurakintachua@gmail.com>
2017-05-26 16:05:48 +0700

    Update bot_facebook.php

--- bot_facebook.php
+++ bot_facebook.php
@@ -1,176 +1,231 @@
 <?php
-$access_token = 'Mb1MfpF5t/adwXJenkK5jMkKvZOcGCOaiLIZzFI2ZcV059EKV4eC0FV4Euf8FPg4XqQcDwoB+7bTMVxxtoeJgAlqAxJfbXcnpzxLX8eiHTIaA6snZz8ZU6i8q9drPExofgFrjUrDH/TuDT/OYenjDgdB04t89/1O/w1cDnyilFU=';
-
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		$userID = $event['source']['userId'];
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent			
-			error_log('message '.$event['message']);
-			$text = $event['message']['text'];
-			//$text = 'hello world';
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			error_log('replyToken'.$replyToken);
-			// Build message to reply back
-			// make GET
-			$sessionID = session_id();
-			error_log('session '.$sessionID);
-			$postCXP = json_encode($para);
-			$cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@System&message='.$text.'&vsDriver=164&channel=line&sessionID='.$userID;
-			
-			
-			error_log($cxpUrl);
-			$chcxp = curl_init($cxpUrl);
-			
-			curl_setopt($chcxp, CURLOPT_CUSTOMREQUEST, "GET");
-			curl_setopt($chcxp, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($chcxp, CURLOPT_FOLLOWLOCATION, 1);
-			$xcpResult = curl_exec($chcxp);
-			curl_close($chcxp);
-			error_log($xcpResult);	
-			//error_log(':'.substr($xcpResult,0,6).':');
-			//error_log('XXXX:'.substr($xcpResult,0,27).'');
-			
-			// inint value
-			$messages = '';
-			$imageURL = '';
-			$title = '';
-			$subTitle = '';
-			$titleButton = '';
-			$webURL = '';
-	
-			// convert value 
-			
-			$resultMes = explode("\n",$xcpResult);
-			
-			for($i = 0; $i < count($resultMes) ; $i++){
-
-				if(substr($resultMes[$i],0,1) == "!"){
-					$imageURL  = trim($resultMes[$i],"!");
-
-				}elseif (substr($resultMes[$i],0,1) == "["){
-					$title  = trim($resultMes[$i],"[");
-
-				}elseif (substr($resultMes[$i],0,1) == "{"){
-					$subTitle   = trim($resultMes[$i],"{");
-
-				}elseif (substr($resultMes[$i],0,1) == "*"){
-					$titleButton   = trim($resultMes[$i],"*");
-
-				}elseif (substr($resultMes[$i],0,1) == "#"){
-					$webURL    = trim($resultMes[$i],"#");
-
-				}else{
-					echo "Not have condition fix.";
-				}
-			}
-			
-			// set message to type
-			
-			$symResult = "";
-			$symImageURL = "!";
-			// 40 charpter
-			$symTitle = "[";
-			// 40 charpter
-			$symSubtitle = "{";
-			$symTitleBN = "*";
-			$symWebURL = "#";
-// 			$symMessOnly = "(";
-
-			foreach ($resultMes as $value) {
-			    $symResult .= substr($value, 0, 1);
-			}
-			
-			$checkImageURL = strpos($symResult, $symImageURL);
-			$checkTitle = strpos($symResult, $symTitle);
-			$checkSubtitle = strpos($symResult, $symSubtitle);
-			$checkTitleBN = strpos($symResult, $symTitleBN);
-			$checkWebURL = strpos($symResult, $symWebURL);
-			$checkMessOnly = strpos($symResult, $symMessOnly);
-
-			
-			if (($checkImageURL !== false) && ($checkTitle !== false) && ($checkSubtitle !== false) && ($checkTitleBN !== false) && ($checkWebURL !== false)) {
-				
-			    	$messages=['type'=> 'template',
-					'altText' => 'this is a buttons template',
-					'template' => [
-						'type'=> 'buttons',
-						'thumbnailImageUrl'=> $imageURL,
-						'title' => $title,
-						'text' => $subTitle,
-						'actions' => [
-								 ['type' => 'uri',
-									'label' => $titleButton,
-									'uri' => $webURL
-								  ]
-							     ]
-							]
-					];
-			}
-			
-
-			}elseif (($checkTitle !== false) && ($checkSubtitle !== false) && ($checkTitleBN !== false) && ($checkWebURL !== false)) {
-
-			    echo "Template not have image";
-
-			}elseif (($checkImageURL !== false) && ($checkTitle !== false) && ($checkSubtitle !== false)) {
-			    echo "Template not have button";
-
-			}elseif (($checkImageURL !== false) && ($checkSubtitle !== false)) {
-			    echo "Template not have title";
-
-			}elseif (($checkImageURL !== false) && ($checkTitle !== false)) {
-			    echo "Template not have subtitle and button";
-
-			}elseif (($checkImageURL !== false)) {
-			    echo "Send image only";
-
-			}elseif (($checkMessOnly !== false)) {
-			    echo "Send message only";
-
-			}else{
-				$messages = [
-				'type' => 'text',
-				'text' => $xcpResult];
-			
-				//ho "Not have condition fix.";
-			}
-			
-			
-			
-		
-			error_log('message : '.$messages);	
-			// Make a POST Request to Messaging API to reply to sender
-			 $url = 'https://api.line.me/v2/bot/message/reply';
-			
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
-}
-echo "OK";
-?>
+error_log('facebook hook ');
+ 
+   $access_token = 'EAASvNkXVo7wBAAZCAZBU4dJBXMCWnoCFCx9sgpLMVZAnUMb3JbqlYehI2bu2rblrSPVurMZCYgqb4mIdHCfVZAz6jcE3aAhQPUjU8CqFiwSJbRZC6SlkZB5WPprBhbwrc5Q2bAE7Az9P1ukzAAYnvBZCWlZCENEd1N7ZAXhC43aZCtiKAZDZD';
+   $verify_token = 'cxp_poc';
+   $hub_verify_token = null;
+ 
+   if(isset($_REQUEST['hub_challenge'])) {
+     	$challenge = $_REQUEST['hub_challenge'];
+    	$hub_verify_token = $_REQUEST['hub_verify_token'];
+   }
+ 
+  error_log('hub_verify_token  '. $hub_verify_token );
+   if ($hub_verify_token === $verify_token) {
+     	error_log('challenge '.$challenge);
+   }
+ 
+   $input = json_decode(file_get_contents('php://input'), true);
+  error_log('input  '. $input );
+   $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+  error_log('sender  '. $sender );
+   $message = $input['entry'][0]['messaging'][0]['message']['text'];
+  error_log('XXXXXmessage :  '. $message );
+ 	if($message==''){
+ 		return;
+ 	}
+ $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@System&message='.$message.'&vsDriver=164&channel=facebook&sessionID=EAASvNkXVo7wBAAZCAZBU4dJBXMCWnoCF';
+ 					
+ 	error_log($cxpUrl);
+ 	$chcxp = curl_init($cxpUrl);
+ 
+ 	curl_setopt($chcxp, CURLOPT_CUSTOMREQUEST, "GET");
+ 	curl_setopt($chcxp, CURLOPT_RETURNTRANSFER, true);
+ 	curl_setopt($chcxp, CURLOPT_FOLLOWLOCATION, 1);
+ 	$xcpResult = curl_exec($chcxp);
+ 	curl_close($chcxp);
+ 	error_log('cxp '.$chcxp);
+ 
+ 	//API Url
+ 	$url = 'https://graph.facebook.com/v2.6/me/messages';
+ 	$message_to_reply = $xcpResult;
+ 	error_log('url reply'.$url);
+ 	//Initiate cURL.
+ 	$ch = curl_init($url);
+ 
+ // check if
+  	$messages = '';
+ 	if(strstr($message_to_reply) == '<,[,{,*,#'){
+ 		error_log('---XXXXXXX test new function');
+ 	}
+ 
+	 if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/b7e'){
+  		if(substr($message_to_reply,0,27)=='  <https://www.mx7.com/i/b7'){
+  			error_log('pic');
+  			$messages=[
+  				'attachment' =>['type' => 'template',
+ 						'payload' => ['template_type' => 'generic',
+ 							      	'elements' => [
+ 										 [
+ 											'title' => 'Today - 30 Jun 17',
+ 											'image_url'=> 'https://www.mx7.com/i/b7e/HdD0Yj.jpg',
+ 											'subtitle' => 'Everyday Special,Get Cash Back Up to 17%,4% cash back for other K-Credit Cards',
+ 											'buttons' => [
+ 												['type' => 'web_url',
+ 												'title' => 'More information',
+ 												'url' => 'https://www.kasikornbank.com/EN/promotion/Pages/Supermarket.aspx'
+ 												]
+ 											]
+ 										]
+  									]
+ 							      
+ 							      ]
+  						]
+  				];
+ 	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/b7b'){
+  			error_log('pic');
+  			$messages=[
+  				'attachment' =>['type' => 'template',
+ 						'payload' => ['template_type' => 'generic',
+ 								'elements' => [
+ 										 [
+ 											'title' => 'Shori Sushi House',
+ 											'image_url'=> 'https://www.mx7.com/i/b7b/CQ6y5K.png',
+ 											'subtitle' => 'Opening Hours: 11:00 – 23:00',
+ 											'buttons' => [
+ 												['type' => 'web_url',
+ 												'title' => 'Location',
+ 												'url' => 'https://goo.gl/maps/jxgfN1aXYzR2'
+ 												],
+ 												[
+ 												  'type' => 'phone_number',
+ 												  'title' => 'Call',
+ 												  'payload' => '+662-169-1532'
+ 											       ]
+ 											]
+ 										]
+  									]
+ 							      
+ 							      ]
+ 
+  						]
+  				];
+ 	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/bde'){
+ 		error_log('pic');
+ 		$messages=[
+ 			'attachment' =>['type' => 'template',
+ 					'payload' => ['template_type' => 'generic',
+ 						      		'elements' => [
+ 										[
+ 											'title' => 'Hakone Bangkok',
+ 											'image_url'=> 'https://www.mx7.com/i/bde/2oPh6u.png',
+ 											'subtitle' => 'Opening Hours: 10:00 – 22:00',
+ 											'buttons' => [
+ 												['type' => 'web_url',
+ 												'title' => 'Location',
+ 												'url' => 'https://goo.gl/maps/EJQUDLLtugE2'
+ 												],
+ 												[
+ 												  'type' => 'phone_number',
+ 												  'title' => 'Call',
+ 												  'payload' => '+662-108-2790'
+ 											       ]
+ 											]
+ 										]
+ 									]
+ 						      
+ 						      ]
+ 					]
+				];
+	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/1f1'){
+ 	error_log('pic');
+ 	$messages=[
+ 		'attachment' =>['type' => 'template',
+ 				'payload' => ['template_type' => 'generic',
+ 					      	'elements' => [
+ 								 [
+ 									'title' => 'Yoshino Yama',
+ 									'image_url'=> 'https://www.mx7.com/i/1f1/79drKy.png',
+ 									'subtitle' => 'Opening Hours: 17:00 – 01:00',
+ 									'buttons' => [
+ 										['type' => 'web_url',
+ 										'title' => 'Location',
+ 										'url' => 'https://goo.gl/maps/FgVPc8yddDo'
+ 										],
+ 										[
+ 										  'type' => 'phone_number',
+ 										  'title' => 'Call',
+ 										  'payload' => '+662-259-2582'
+ 									       ]
+ 									]
+ 								]
+ 							]
+ 					      
+ 					      ]
+ 				]
+ 		];
+ 	}else if(substr($message_to_reply,0,27)=='  https://www.skyscanner.ne'){
+ 	error_log('pic');
+ 	$messages=[
+ 		'attachment' =>['type' => 'template',
+ 				'payload' => ['template_type' => 'generic',
+ 					      	'elements' => [
+ 								 [
+ 									'title' => 'Skyscanner Flights',
+ 									'subtitle' => 'Skyscanner compares millions of flights to find you the cheapest deal, fast.',
+ 									'buttons' => [
+ 										['type' => 'web_url',
+ 										'title' => 'Check It',
+ 										'url' => 'https://www.skyscanner.net/'
+ 										]
+ 									]
+ 								]
+ 							]
+ 					      
+ 					      ]
+ 				]
+ 		];
+ 	}else if(substr($message_to_reply,0,27)=='  https://www.hotelscombine'){
+ 	error_log('pic');
+ 	$messages=[
+ 		'attachment' =>['type' => 'template',
+ 				'payload' => ['template_type' => 'generic',
+ 					      	'elements' => [
+ 								 [
+ 									'title' => 'Hotels Combined',
+ 									'buttons' => [
+ 										['type' => 'web_url',
+ 										'title' => 'Check It',
+ 										'subtitle' => 'The best hotel deals from all the top travel sites. Guaranteed',
+ 										'url' => 'https://www.hotelscombined.co.th'
+ 										]
+ 									]
+ 								]
+ 							]
+ 					      
+ 					      ]
+ 				]
+ 		];
+ 						
+  	}else{	
+ 		$messages = [
+ 			'text' => $message_to_reply
+  		];
+  	}
+ 
+ 
+ //The JSON data.
+ 	$jsonData = [
+ 	    'access_token'=>$access_token,
+ 	    'recipient'=>[
+ 		'id'=> $sender
+ 	      ],
+ 	    'message'=> $messages
+ 		    
+ 	];
+ 
+ 	//Encode the array into JSON.
+ 	$jsonDataEncoded = json_encode($jsonData);
+ 	//Tell cURL that we want to send a POST request.
+ 	curl_setopt($ch, CURLOPT_POST, 1);
+ 	//Attach our encoded JSON string to the POST fields.
+ 	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+ 	//Set the content type to application/json
+ 	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+ 
+ 	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+ 	//Execute the request
+ 	if(!empty($input['entry'][0]['messaging'][0]['message'])){
+ 	    $result = curl_exec($ch);
+	 }
+ ?>
