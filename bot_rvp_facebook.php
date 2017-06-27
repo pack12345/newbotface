3eb1311a9f9641e1d501382a7d7904b661549027
pack12345 <anurakintachua@gmail.com>
2017-06-27 11:37:38 +0700

    Update bot_rvp_facebook.php

--- bot_rvp_facebook.php
+++ bot_rvp_facebook.php
@@ -14,6 +14,7 @@ error_log('facebook hook ');
    if ($hub_verify_token === $verify_token) {
      	error_log('challenge '.$challenge);
    }
+error_log('request : '.$_REQUEST.toString());
 $input = json_decode(file_get_contents('php://input'), true);
 error_log($input);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
