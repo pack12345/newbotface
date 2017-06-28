d380841797ff50db708032ba98c538b74ef0732c
pack12345 <anurakintachua@gmail.com>
2017-06-28 15:57:01 +0700

    Update bot_rvp_facebook.php

--- bot_rvp_facebook.php
+++ bot_rvp_facebook.php
@@ -90,7 +90,13 @@ error_log('facebook hook ');
 			$checkWebURL = strpos($symResult, $symWebURL);
 			$checkMessOnly = strpos($symResult, $symMessOnly);
 
-			error_log($checkImageURL,$checkTitle,$checkSubtitle,$checkTitleBN,$checkWebURL,$checkMessOnly);
+				error_log($checkImageURL);
+				error_log($checkTitle);
+				error_log($checkSubtitle);
+				error_log($checkTitleBN);
+				error_log($checkWebURL);
+				error_log($checkMessOnly);
+
 			if (($checkImageURL !== false) && ($checkTitle !== false) && ($checkSubtitle !== false) && ($checkTitleBN !== false) && ($checkWebURL !== false)) {
 			    error_log("Template have all");
 			$messages=[
