d0c97dc35758a035c9218c1e008371fc6019f5a2
pack12345 <anurakintachua@gmail.com>
2017-06-28 10:04:11 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -5,6 +5,8 @@ error_log('facebook hook ');
 $access_token = 'EAAGJ1qglvHoBAG4ZAcu21bWCnsHwxAJgolI1xZCGOgwlPdyZBv54QMQtB0UKf63jIMOPo3UZCVb180S8lf8ZC1hjNwrP1QP09IhE5hfCbbw5H2DqMBR9sxZCVKy1UaR7zl8WCZCh9iOEGtpXIZAZBigpMe1QixZC4Wz9zjB6voUTJhNnAUwlzKe38M';
 $verify_token === 'rvp_poc';
    
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
 
 
