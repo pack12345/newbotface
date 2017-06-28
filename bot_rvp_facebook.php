4660c579ca1ca8447c138e54858cded0a2312767
pack12345 <anurakintachua@gmail.com>
2017-06-28 13:59:28 +0700

    Update bot_rvp_facebook.php

--- bot_rvp_facebook.php
+++ bot_rvp_facebook.php
@@ -2,24 +2,29 @@
 error_log('facebook hook ');
  
    $access_token = 'EAAYCbn0ZBGEoBABtZC3PexrdHUPsB2bSsiPIl7cw6fIMeZC06uTofO3Ch9lemiwKEyjsi4iiFPi0OyoZAmslRHfFPU5yeoq3w2M4ZB4Gly5etZCGo30PpbxeAz8tTiL8f8XnKnGXzjqbnktw7dY4jnSRw9671AA0XhkCv9pfY2tAZDZD';
-   
+   $verify_token = 'rvp_poc';
+   $hub_verify_token = null;
 
-$challenge = $_REQUEST['hub_challenge'];
-$verify_token = $_REQUEST['hub_verify_token'];
-
-if ($verify_token === 'rvp_poc') {
-error_log( 'challenge'.$challenge);
-}
-
-error_log('data : '.file_get_contents('php://input'));
-$input = json_decode(file_get_contents('php://input'), true);
-error_log($input);
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-error_log($sender);
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
-error_log($message);
-
- $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@System&message='.$message.'&vsDriver=164&channel=facebookrvp&sessionID=EAABvD2ZBmpBkBAOhgmbcNhW024y';
+   if(isset($_REQUEST['hub_challenge'])) {
+     	$challenge = $_REQUEST['hub_challenge'];
+    	$hub_verify_token = $_REQUEST['hub_verify_token'];
+   }
+ 
+  error_log('hub_verify_token  '. $hub_verify_token );
+   if ($hub_verify_token === $verify_token) {
+     	error_log('challenge '.$challenge);
+   }
+ 
+   $input = json_decode(file_get_contents('php://input'), true);
+  error_log('input  '. $input );
+   $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+  error_log('sender  '. $sender );
+   $message = $input['entry'][0]['messaging'][0]['message']['text'];
+  error_log('XXXXXmessage :  '. $message );
+ 	if($message==''){
+ 		return;
+ 	}
+ $cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@System&message='.$message.'&vsDriver=164&channel=facebook&sessionID=EAASvNkXVo7wBAAZCAZBU4dJBXMCWnoCF';
  					
  	error_log($cxpUrl);
  	$chcxp = curl_init($cxpUrl);
@@ -37,107 +42,8 @@ error_log($message);
  	error_log('url reply'.$url);
  	//Initiate cURL.
  	$ch = curl_init($url);
- 
- // check if
-  	$messages = '';
-
-	 if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/bbb'){
-  		
-  			error_log('pic');
-  			$messages=[
-  				'attachment' =>['type' => 'template',
- 						'payload' => ['template_type' => 'generic',
- 							      	'elements' => [
- 										 [
- 											'title' => 'Today - 30 Jun 17',
- 											'image_url'=> 'https://www.mx7.com/i/bbb/G5xC6I.png',
- 											'subtitle' => 'Everyday Special,Get Cash Back Up to 17%,4% cash back for other K-Credit Cards',
- 											'buttons' => [
- 												['type' => 'web_url',
- 												'title' => 'More information',
- 												'url' => 'https://www.kasikornbank.com/EN/promotion/Pages/Supermarket.aspx'
- 												]
- 											]
- 										]
-  									]
- 							      
- 							      ]
-  						]
-  				];
- 	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/b7b'){
-  			error_log('pic');
-  			$messages=[
-  				'attachment' =>['type' => 'template',
- 						'payload' => ['template_type' => 'generic',
- 								'elements' => [
- 										 [
- 											'title' => 'Shori Sushi House',
- 											'image_url'=> 'https://www.mx7.com/i/b7b/CQ6y5K.png',
- 											'subtitle' => 'Opening Hours: 11:00 – 23:00',
- 											'buttons' => [
- 												['type' => 'web_url',
- 												'title' => 'Location',
- 												'url' => 'https://goo.gl/maps/jxgfN1aXYzR2'
- 												],
- 												[
- 												  'type' => 'phone_number',
- 												  'title' => 'Call',
- 												  'payload' => '+662-169-1532'
- 											       ]
- 											]
- 										]
-  									]
- 							      
- 							      ]
- 
-  						]
-  				];
- 	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/bde'){
- 		error_log('pic');
- 		$messages=[
- 			'attachment' =>['type' => 'template',
- 					'payload' => ['template_type' => 'generic',
- 						      		'elements' => [
- 										[
- 											'title' => 'Hakone Bangkok',
- 											'image_url'=> 'https://www.mx7.com/i/bde/2oPh6u.png',
- 											'subtitle' => 'Opening Hours: 10:00 – 22:00',
- 											'buttons' => [
- 												['type' => 'web_url',
- 												'title' => 'Location',
- 												'url' => 'https://goo.gl/maps/EJQUDLLtugE2'
- 												],
- 												[
- 												  'type' => 'phone_number',
- 												  'title' => 'Call',
- 												  'payload' => '+662-108-2790'
- 											       ]
- 											]
- 										]
- 									]
- 						      
- 						      ]
- 					]
-				];
-	}else if(substr($message_to_reply,0,27)=='  https://www.mx7.com/i/1f1'){
- 	error_log('pic');
- 	$messages=[
- 		'attachment' =>['type' => 'template',
- 				'payload' => ['template_type' => 'generic',
- 					      	'elements' => [
- 								 [
- 									'title' => 'Yoshino Yama',
- 									'image_url'=> 'https://www.mx7.com/i/1f1/79drKy.png',
- 									'subtitle' => 'Opening Hours: 17:00 – 01:00',
- 									'buttons' => [
- 										['type' => 'web_url',
- 										'title' => 'Location',
- 										'url' => 'https://goo.gl/maps/FgVPc8yddDo'
- 										],
- 										[
- 										  'type' => 'phone_number',
- 										  'title' => 'Call',
-$messages = '';
+	
+			$messages = '';
 			
 			$result = explode("\n",$message_to_reply);
 			$symResult = "";
@@ -304,7 +210,6 @@ $messages = '';
 					];
 			}
  
- error_log('before response');
  //The JSON data.
  	$jsonData = [
  	    'access_token'=>$access_token,
@@ -327,6 +232,6 @@ $messages = '';
  	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
  	//Execute the request
  	if(!empty($input['entry'][0]['messaging'][0]['message'])){
- 	    $result = curl_exec($ch);
+ 	    $resultMes = curl_exec($ch);
 	 }
  ?>
