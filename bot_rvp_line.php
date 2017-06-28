22ba97d728e114936d913b9da20f91052814a787
pack12345 <anurakintachua@gmail.com>
2017-06-28 12:37:19 +0700

    Update bot_rvp_line.php

--- bot_rvp_line.php
+++ bot_rvp_line.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			$sessionID = session_id();
 			error_log('session '.$sessionID);
 			$postCXP = json_encode($para);
-			$cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@System&message='.$text.'&vsDriver=164&channel=linervp&sessionID='.$userID;
+			$cxpUrl = 'http://58.82.133.74:8070/VoxeoCXP/DialogMapping?VSN=testService@System&message='.$text.'&vsDriver=164&channel=line&sessionID='.$userID;
 			
 			
 			error_log($cxpUrl);
@@ -41,118 +41,137 @@ if (!is_null($events['events'])) {
 			error_log('XXXX:'.substr($xcpResult,0,27).'');
 			$messages = '';
 			
-		if(substr($xcpResult,0,27)=="  https://www.mx7.com/i/bbb"){
-			error_log('pic');
-	
-			$messages=['type'=> 'template',
-					'altText' => 'this is a buttons template',
-					'template' => [
-						'type'=> 'buttons',
-						'thumbnailImageUrl'=> 'https://www.mx7.com/i/bbb/G5xC6I.png',
-						'text' => 'Contact Center at  0-2100-9191',
-						'actions' => [
-
-								 ['type' => 'uri',
-									'label' => 'More information',
-									'uri' => 'http://www.rvp.co.th/ClaimQA.php'
-								  ]
-							     ]
-
-							]
-				];
-
-			}	
-			 else if(substr($xcpResult,0,27)=="  https://www.mx7.com/i/184"){
-				error_log('pic');
-		
-				$messages=[
-  					'type'=> 'template',
-  					'altText' => 'this is a buttons template',
- 					'template' => [
-						'type'=> 'buttons',
-						'thumbnailImageUrl'=> 'https://www.mx7.com/i/184/jxJwgZ.png',
-						'text' => 'Contact Center at  0-2100-9191',
-						'actions' => [
-
-								 ['type' => 'uri',
-									'label' => 'More information',
-									'uri' => 'http://www.rvp.co.th/makingaclaim.php'
-								  ]
-							     ]
-
-							]
-					];
-			}	
-			else if(substr($xcpResult,0,27)=="  https://www.mx7.com/i/b1f"){
-				error_log('pic');
-		
-				$messages=[
-  					'type'=> 'template',
-  					'altText' => 'this is a buttons template',
- 					'template' => [
-						'type'=> 'buttons',
-						'thumbnailImageUrl'=> 'https://www.mx7.com/i/b1f/WuFfPp.png',
-						'text' => 'Contact Center at  0-2100-9191',
-						'actions' => [
-
-								 ['type' => 'uri',
-									'label' => 'More information',
-									'uri' => 'http://www.rvp.co.th/makingaclaim.php'
-								  ]
-							     ]
-
-							]
-					];	
-			}	
-			else if(substr($xcpResult,0,27)=="  https://www.mx7.com/i/bc1"){
-				error_log('pic');
-		
-				$messages=[
-  					'type'=> 'template',
-  					'altText' => 'this is a buttons template',
- 					'template' => [
-						'type'=> 'buttons',
-						'thumbnailImageUrl'=> 'https://www.mx7.com/i/bc1/PNwwLS.png',
-						'text' => 'Contact Center at  0-2100-9191',
-						'actions' => [
-
-								 ['type' => 'uri',
-									'label' => 'More information',
-									'uri' => 'http://www.rvp.co.th/makingaclaim.php'
-								  ]
-							     ]
-
-							]
-					];	
-			}	
-			else if(substr($xcpResult,0,27)=="  https://www.mx7.com/i/b5a"){
-				error_log('pic');
-		
-				$messages=[
-  					'type'=> 'template',
-  					'altText' => 'this is a buttons template',
- 					'template' => [
-						'type'=> 'buttons',
-						'thumbnailImageUrl'=> 'https://www.mx7.com/i/b5a/pjfxyj.png',
-						'text' => 'Contact Center at  0-2100-9191',
-						'actions' => [
-
-								 ['type' => 'uri',
-									'label' => 'More information',
-									'uri' => 'http://www.rvp.co.th/makingaclaim.php'
-								  ]
-							     ]
-
-							]
-					];	
-		
-		
-			}else{	
-				$messages = [
-					'type' => 'text',
-					'text' => $xcpResult
-				];
+			$result = explode("\n",$xcpResult);
+			$symResult = "";
+			foreach ($result as $value) {
+			    $symResult .= substr($value, 0, 1);
 			}
+			$imageURL = "";
+			$title = "";
+			$subTitle = "";
+			$titleButton = "";
+			$webURL = "";
+			$messages = "";
+			for($i = 0; $i < count($result) ; $i++){
+				if(substr($result[$i],0,1) == "!"){
+					$imageURL  = trim($result[$i],"!");
+					 error_log($imageURL);
+				}elseif (substr($result[$i],0,1) == "["){
+					$title  = trim($result[$i],"[");
+					error_log($title);
+				}elseif (substr($result[$i],0,1) == "{"){
+					$subTitle   = trim($result[$i],"{");
+					error_log($subTitle);
+				}elseif (substr($result[$i],0,1) == "*"){
+					$titleButton   = trim($result[$i],"*");
+					error_log($titleButton);
+				}elseif (substr($result[$i],0,1) == "#"){
+					$webURL    = trim($result[$i],"#");
+					error_log($webURL);
+				}else{
+					error_log("Not have condition fix2.");
+					$messageDir = implode("\n", $result);
+				}
+			}
+			$symImageURL = "!";
+			$symTitle = "[";
+			$symSubtitle = "{";
+			$symTitleBN = "*";
+			$symWebURL = "#";
+			$symMessOnly = "(";
+			$checkImageURL = strpos($symResult, $symImageURL);
+			$checkTitle = strpos($symResult, $symTitle);
+			$checkSubtitle = strpos($symResult, $symSubtitle);
+			$checkTitleBN = strpos($symResult, $symTitleBN);
+			$checkWebURL = strpos($symResult, $symWebURL);
+			$checkMessOnly = strpos($symResult, $symMessOnly);
+			if (($checkImageURL !== false) && ($checkTitle !== false) && ($checkSubtitle !== false) && ($checkTitleBN !== false) && ($checkWebURL !== false)) {
+			    error_log("Template have all");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'thumbnailImageUrl'=> $imageURL,
+									'title' => $title,
+									'text' => $subTitle,
+									'actions' => [
+											 ['type' => 'uri',
+												'label' => $titleButton,
+												'uri' => $webURL
+											  ]
+										     ]
+										]
+								];
+			}elseif (($checkTitle !== false) && ($checkSubtitle !== false) && ($checkTitleBN !== false) && ($checkWebURL !== false)) {
+			    error_log("Template not have image");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'title' => $title,
+									'text' => $subTitle,
+									'actions' => [
+											 ['type' => 'uri',
+												'label' => $titleButton,
+												'uri' => $webURL
+											  ]
+										     ]
+										]
+								];
+			}elseif (($checkImageURL !== false) && ($checkTitle !== false) && ($checkSubtitle !== false)) {
+			    error_log("Template not have button");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'thumbnailImageUrl'=> $imageURL,
+									'title' => $title,
+									'text' => $subTitle
+										]
+								];
+			}elseif (($checkImageURL !== false) && ($checkSubtitle !== false)) {
+			    error_log("Template not have title");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'thumbnailImageUrl'=> $imageURL,
+									'text' => $subTitle
+										]
+								];
+			}elseif (($checkImageURL !== false) && ($checkTitle !== false)) {
+			    error_log("Template not have subtitle and button");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'thumbnailImageUrl'=> $imageURL,
+									'title' => $title
+										]
+								];
+			}elseif (($checkImageURL !== false)) {
+			    error_log("Send image only");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'thumbnailImageUrl'=> $imageURL
+										]
+								];
+			}elseif (($checkMessOnly !== false)) {
+			    error_log("Send message only");
+					$messages = [
+						'type' => 'text',
+						'text' => $messageDir
+					];
+			}else{
+				error_log("Not have condition fix.");
+				$messages = [
+						'type' => 'text',
+						'text' => $messageDir
+					];
+			}
+			
 			error_log('message : '.$messages);	
 			// Make a POST Request to Messaging API to reply to sender
 			 $url = 'https://api.line.me/v2/bot/message/reply';
@@ -163,17 +182,15 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
+			$resultMes = curl_exec($ch);
 			curl_close($ch);
-
-			echo $result . "\r\n";
+			echo $resultMes . "\r\n";
 		}
 	}
 }
