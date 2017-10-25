1fa1f987a6d59f0fc099391c94cb6256c37051ab
pack12345 <anurakintachua@gmail.com>
2017-10-25 17:14:14 +0700

    Update bot_airline_line.php

--- bot_airline_line.php
+++ bot_airline_line.php
@@ -51,8 +51,16 @@ if (!is_null($events['events'])) {
 			error_log($xcpResult);	
 			error_log('XXXX:'.substr($xcpResult,0,27).'');
 			
-			if($xcpResult == "  https://www.picz.in.th/im"){
+			if(substr($xcpResult,0,27) == "  https://www.picz.in.th/im"){
 			    error_log("Template have all");
+				$messages=['type'=> 'template',
+								'altText' => 'this is a buttons template',
+								'template' => [
+									'type'=> 'buttons',
+									'thumbnailImageUrl'=> 'https://www.picz.in.th/images/2017/10/24/20150828-230116.jpg'
+									
+										]
+								];
 				
 			}
 			
