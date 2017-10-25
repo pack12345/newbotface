4e33c81826cdf6ae67d89cd47e5a95a960b5172d
pack12345 <anurakintachua@gmail.com>
2017-10-25 10:41:41 +0700

    Update bot_airline_line.php

--- bot_airline_line.php
+++ bot_airline_line.php
@@ -42,6 +42,7 @@ if (!is_null($events['events'])) {
 				    "VSN=testService@System&message='.$text.'&vsDriver=164&channel=line&sessionID='.$userID");
 			
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			error_log("url_post : ".$ch);
 			$xcpResult = curl_exec($ch);
 			
