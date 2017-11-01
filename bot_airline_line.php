74fd00d8942dd58c7a5850a3e594ffe1efa3ef87
pack12345 <anurakintachua@gmail.com>
2017-11-01 14:47:19 +0700

    Update bot_airline_line.php

--- bot_airline_line.php
+++ bot_airline_line.php
@@ -2,11 +2,11 @@
 ob_start();
 $access_token = 'IHf9TGAiHOH3XZdKNdrz+NBHzcPr2y+f2rpdiDj7b2okT11aW2a7eknIfMCVkkIekN82nmiUonCyubOwPxCD0WN6ObtI8miTVkemgWQN8M27m8kCdxcbE6Q/rGRExajPhaWfpzyrO8xTyGyIrE/TGgdB04t89/1O/w1cDnyilFU=';
 // $ch_session = $_COOKIE['departure'];
-if(isset($_COOKIE["departure"])) {
+if(isset($_COOKIE['user_id'])) {
 setcookie('user_id', 1, time()+3600, '/');
-error_log('noss:'.$_COOKIE['user_id'];);
+error_log('noss:'.$_COOKIE['user_id']);
 }
-error_log('ss:'.$_COOKIE['user_id'];);
+error_log('ss:'.$_COOKIE['user_id']);
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
