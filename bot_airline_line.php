d3c4d2d3bda894ccd6e33bdfa9d55345c4d51341
pack12345 <anurakintachua@gmail.com>
2017-11-01 12:19:23 +0700

    Update bot_airline_line.php

--- bot_airline_line.php
+++ bot_airline_line.php
@@ -89,7 +89,7 @@ if (!is_null($events['events'])) {
 						'text' => $aaa[1]
 					];
 			}
-			else if(substr($xcpResult,0,27) == "  https://www.picz.in.th/im"){
+			else if(substr($xcpResult,0,27) == "    https://www.picz.in.th/"){
 			    error_log("Send image only");
 				$messages=[
 						  "type"=> "template",
