3d17ddd5f54e1fd9f7d66c009dd84b7d52629478
pack12345 <anurakintachua@gmail.com>
2017-11-02 11:19:10 +0700

    Update airline_cookie.php

--- airline_cookie.php
+++ airline_cookie.php
@@ -3,5 +3,5 @@
 $_COOKIE['ckDeparture'] = $_GET['departure']; 
 $_COOKIE['ckDate'] = $_GET['date']; 
 
-echo '$_GET['departure']'.$_GET['departure'];
-echo '$_GET['date']'.$_GET['date'];
+echo "departure".$_GET['departure'];
+echo "date".$_GET['date'];
