2d7f045511664d9efb1ac2c6878e38421b38cda0
pack12345 <anurakintachua@gmail.com>
2017-11-02 10:48:35 +0700

    Create airline_cookie.php

---
+++ airline_cookie.php
@@ -0,0 +1,9 @@
+<?php
+
+$_COOKIE['ckDeparture'] = $_GET['departure']; 
+$_COOKIE['ckDate'] = $_GET['date']; 
+
+echo "$_GET['departure']".$_GET['departure'];
+echo "$_GET['date']".$_GET['date'];
+
+?>
