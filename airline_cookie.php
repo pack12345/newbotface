beb53ddf95080496557cfd7f80fa9040a1ffb786
pack12345 <anurakintachua@gmail.com>
2017-11-02 17:31:01 +0700

    Update airline_cookie.php

--- airline_cookie.php
+++ airline_cookie.php
@@ -8,10 +8,13 @@
 // $_COOKIE['ckDate'] = 2; 
 // setcookie('departure',$_GET['departure']);
 // setcookie('date',$_GET['date'],time()+3600, "/");
-$_COOKIE['departure'] = $_GET['departure'];
-$_COOKIE['date'] = $_GET['date'];
+// $_COOKIE['departure'] = $_GET['departure'];
+// $_COOKIE['date'] = $_GET['date'];
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 
 error_log('--$_GET error : '.$_GET['departure']);
 error_log('--$_GET error : '.$_GET['date']);
-error_log('--$sDeparture error : '.$_COOKIE['departure']);
+error_log('--$sDeparture error : '.$_COOKIE[$cookie_name]);
 error_log('--$sDate error : '.$_COOKIE['date']);
