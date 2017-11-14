693f8ff36eb957bddd5945fa9196ba3d5e5d7442
pack12345 <anurakintachua@gmail.com>
2017-11-14 16:24:16 +0700

    Update bot_Airline_facebook.php

--- bot_Airline_facebook.php
+++ bot_Airline_facebook.php
@@ -3,17 +3,17 @@ error_log('facebook hook ');
  
    $access_token = 'EAAcCIIH2scoBAIraN46Dv06N41dlZC5E5gkKcwZBuU0mXS6wK71pnThMs97An0ZAnNCauHv0qv8TZBVaHyur9JjWUvrem5EkXpE867SlQbuSKdic9lfM5zZA3xz956pfahV24mHDIHKhSsBLUAEZA17OXg2vZBaQ5nGBcRxZBdpAOgZDZD';
    $verify_token = 'airline_bot';
- //  $hub_verify_token = null;
+   $hub_verify_token = null;
  
-//    if(isset($_REQUEST['hub_challenge'])) {
-//      	$challenge = $_REQUEST['hub_challenge'];
-//     	$hub_verify_token = $_REQUEST['hub_verify_token'];
-//    }
+   if(isset($_REQUEST['hub_challenge'])) {
+     	$challenge = $_REQUEST['hub_challenge'];
+    	$hub_verify_token = $_REQUEST['hub_verify_token'];
+   }
  
-//   error_log('hub_verify_token  '. $hub_verify_token );
-//    if ($hub_verify_token === $verify_token) {
-//      	error_log('challenge '.$challenge);
-//    }
+   error_log('hub_verify_token  '. $hub_verify_token );
+   if ($hub_verify_token === $verify_token) {
+     	error_log('challenge '.$challenge);
+   }
  
 //    $input = json_decode(file_get_contents('php://input'), true);
 //   error_log('input  '. $input );
