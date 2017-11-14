5fdd75e37dc32fe5c9ed1171ad1a140bc5defefe
pack12345 <anurakintachua@gmail.com>
2017-11-14 16:19:57 +0700

    Update bot_Airline_facebook.php

--- bot_Airline_facebook.php
+++ bot_Airline_facebook.php
@@ -3,7 +3,7 @@ error_log('facebook hook ');
  
    $access_token = 'EAAcCIIH2scoBAIraN46Dv06N41dlZC5E5gkKcwZBuU0mXS6wK71pnThMs97An0ZAnNCauHv0qv8TZBVaHyur9JjWUvrem5EkXpE867SlQbuSKdic9lfM5zZA3xz956pfahV24mHDIHKhSsBLUAEZA17OXg2vZBaQ5nGBcRxZBdpAOgZDZD';
    $verify_token = 'airline_bot';
-   $hub_verify_token = null;
+ //  $hub_verify_token = null;
  
 //    if(isset($_REQUEST['hub_challenge'])) {
 //      	$challenge = $_REQUEST['hub_challenge'];
@@ -11,9 +11,9 @@ error_log('facebook hook ');
 //    }
  
 //   error_log('hub_verify_token  '. $hub_verify_token );
-//    if ($hub_verify_token === $verify_token) {
-//      	error_log('challenge '.$challenge);
-//    }
+   if ($hub_verify_token === $verify_token) {
+     	error_log('challenge '.$challenge);
+   }
  
 //    $input = json_decode(file_get_contents('php://input'), true);
 //   error_log('input  '. $input );
