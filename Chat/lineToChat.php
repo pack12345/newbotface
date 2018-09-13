e91f421d6f101b6d13744d26a58bf608be1d9e19
pack12345 <anurakintachua@gmail.com>
2018-09-14 02:18:46 +0700

    Update lineToChat.php

--- Chat/lineToChat.php
+++ Chat/lineToChat.php
@@ -2,16 +2,16 @@
 
 $log = array();
     
-    if(!empty($_GET["userid"])){
+    if($_GET['userid'] != ""){
         
-		  $nickname = htmlentities(strip_tags($_GET['userid']));
-			 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-			  $message = htmlentities(strip_tags($_GET['text']);
-		 if(($message) != "\n"){
+		  $nickname = $_GET['userid'];#htmlentities(strip_tags($_GET['userid']));
+			# $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+			  $message = $_GET['text'];#htmlentities(strip_tags($_GET['text']);
+		# if(($message) != "\n"){
         	
-			 if(preg_match($reg_exUrl, $message, $url)) {
-       			$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
-				} 
+		#	 if(preg_match($reg_exUrl, $message, $url)) {
+       		#	$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
+		#		} 
 			 if(!file_exists('Agent.txt')){
 				 fwrite(fopen('Agent.txt', 'w'), $_GET['userid'] . "\n");
 				 fclose(fopen('Agent.txt', 'w'));
@@ -19,7 +19,7 @@ $log = array();
 			 
         	
         	 fwrite(fopen('chat.txt', 'a'), "<span>". $nickname . "</span>" . $message = str_replace("\n", " ", $message) . "\n"); 
-		 }
+		 #}
      }
   echo json_encode($log);
 
