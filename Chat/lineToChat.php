bcd1a1aa80519cdd448effd7952f1808e81050f7
pack12345 <anurakintachua@gmail.com>
2018-09-14 02:05:23 +0700

    Update lineToChat.php

--- Chat/lineToChat.php
+++ Chat/lineToChat.php
@@ -1,23 +1,25 @@
 <?php
+
+$log = array();
     
     if(!empty($_GET["userid"])){
         
-		  $nickname = htmlentities(strip_tags($_GET["userid"]));
+		  $nickname = htmlentities(strip_tags($_GET['userid']));
 			 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-			  $message = htmlentities(strip_tags($_GET["text"]);
+			  $message = htmlentities(strip_tags($_GET['text']);
 		 if(($message) != "\n"){
         	
 			 if(preg_match($reg_exUrl, $message, $url)) {
        			$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
 				} 
 			 if(!file_exists('Agent.txt')){
-				 fwrite(fopen('Agent.txt', 'w'), $_GET["userid"] . "\n");
+				 fwrite(fopen('Agent.txt', 'w'), $_GET['userid'] . "\n");
 			 }
 			 
         	
         	 fwrite(fopen('chat.txt', 'a'), "<span>". $nickname . "</span>" . $message = str_replace("\n", " ", $message) . "\n"); 
 		 }
      }
-  echo "to chat".$_GET["userid"].$message;
+  echo json_encode($log);
 
 ?>
