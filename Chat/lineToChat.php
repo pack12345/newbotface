f4d024f18d8e507fb8c5a1c5214a2c863fb958c7
pack12345 <anurakintachua@gmail.com>
2018-09-14 12:38:51 +0700

    Update lineToChat.php

--- Chat/lineToChat.php
+++ Chat/lineToChat.php
@@ -4,7 +4,7 @@ $log = array();
     
     if($_GET['userid'] != ""){
         
-		  $nickname = $_GET['userid'];#htmlentities(strip_tags($_GET['userid']));
+		  $nickname = "LineUser";#htmlentities(strip_tags($_GET['userid']));
 			# $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 			  $message = $_GET['text'];#htmlentities(strip_tags($_GET['text']);
 		# if(($message) != "\n"){
