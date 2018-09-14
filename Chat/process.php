a28cd096631f33e612683a77abcf79aeef2244ef
pack12345 <anurakintachua@gmail.com>
2018-09-14 11:00:29 +0700

    Update process.php

--- Chat/process.php
+++ Chat/process.php
@@ -43,15 +43,16 @@
 		  $nickname = htmlentities(strip_tags($_POST['nickname']));
 			 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 			  $message = htmlentities(strip_tags($_POST['message']));
+		    if(file_exists("Agent.txt")){
+				 $lins = file("Agent.txt");
+				 replyMsg($lins[0],$message);
+			 }
 		 if(($message) != "\n"){
         	
 			 if(preg_match($reg_exUrl, $message, $url)) {
        			$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
 				} 
-			 if(file_exists("Agent.txt")){
-				 $lins = file("Agent.txt");
-				 replyMsg($lins[0],$message);
-			 }
+			 
 			 
         	
         	 fwrite(fopen('chat.txt', 'a'), "<span>". $nickname . "</span>" . $message = str_replace("\n", " ", $message) . "\n"); 
