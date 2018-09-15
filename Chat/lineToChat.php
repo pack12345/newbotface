3e3d4e08c1dd30810d88bfde4a1e826ba82eca5e
pack12345 <anurakintachua@gmail.com>
2018-09-15 22:29:16 +0700

    Update lineToChat.php

--- Chat/lineToChat.php
+++ Chat/lineToChat.php
@@ -7,13 +7,14 @@ $log = array();
 		  $nickname = "LineUser";#htmlentities(strip_tags($_GET['userid']));
 			# $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
 			  $message = $_GET['text'];#htmlentities(strip_tags($_GET['text']);
+	    $agentType = $_GET['type']
 		# if(($message) != "\n"){
         	
 		#	 if(preg_match($reg_exUrl, $message, $url)) {
        		#	$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
 		#		} 
-			 if(!file_exists("Agent.txt")){
-				 $myfile = fopen("Agent.txt", "w") or die("Unable to open file!");
+			 if(!file_exists($agentType)){
+				 $myfile = fopen($agentType, "w") or die("Unable to open file!");
 				 $txt = $_GET['userid'];
 				 fwrite($myfile, $txt);
 				 fclose($myfile);
