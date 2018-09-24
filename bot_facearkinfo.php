f42aa5429119a49c4d4c46cb328a53cb3a6ce5a8
pack12345 <anurakintachua@gmail.com>
2018-09-24 18:02:46 +0700

    Update bot_facearkinfo.php

--- bot_facearkinfo.php
+++ bot_facearkinfo.php
@@ -92,5 +92,20 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
-}
+} 
+
+/*function replyMsg($access_token,$arrayPostData){
+        $strUrl = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+exit; */
 ?>
