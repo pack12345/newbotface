d221f65976fe9f985878d4d355d0bc9e35916752
pack12345 <anurakintachua@gmail.com>
2018-09-26 17:02:42 +0700

    Update bot_facearkinfo.php

--- bot_facearkinfo.php
+++ bot_facearkinfo.php
@@ -72,7 +72,7 @@ $jsonData = '{
 
 $jsonDataEncoded = $jsonData;
 
-  //replyMsg($access_token,$jsonDataEncoded)
+  replyMsg($access_token,$jsonDataEncoded)
 //Tell cURL that we want to send a POST request.
   //curl_setopt($ch, CURLOPT_POST, 1);
 //Attach our encoded JSON string to the POST fields.
