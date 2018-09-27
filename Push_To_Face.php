e15bb66132ff07a9714a4ba64192261c7cee2db8
pack12345 <anurakintachua@gmail.com>
2018-09-27 10:48:29 +0700

    Update Push_To_Face.php

--- Push_To_Face.php
+++ Push_To_Face.php
@@ -47,6 +47,8 @@
     $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
     //Initiate cURL.
     $ch = curl_init($url);
+    //Encode the array into JSON.
+    $jsonDataEncoded = $jsonData;
     //Tell cURL that we want to send a POST request.
     curl_setopt($ch, CURLOPT_POST, 1);
    //Attach our encoded JSON string to the POST fields.
